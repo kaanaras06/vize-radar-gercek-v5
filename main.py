cbf7a43f066b1b06a593e7466e02b8a93a63805c
kaanaras06 <arasgrupdc@gmail.com>
2026-09-25 21:14:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from main_real_scanner import app
-# Railway entry point
+import os
 if __name__ == "__main__":
-    import uvicorn, os
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    import uvicorn
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
